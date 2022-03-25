01dc61bef581eaacc15b027383bdaba9dd0d5fa0
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-03-25 11:58:44 +0100

    update frontend

--- frontend/index.php
+++ frontend/index.php
@@ -1,2 +1,17 @@
 <?php
+    session_start();
+
+    require_once('header.php');
+
+    if (!isset($_SESSION['login'])) {
+        if (isset($_GET['signup'])) {
+            require_once('signup.php');
+            echo "<a href = 'index.php'><h6>Déjà inscrit ? Connectez-vous !</h6></a>";
+        } else {
+            require_once('login.php');
+            echo "<a href = 'index.php?signup'><h6>S'inscrire</h6></a>";
+        }
+    }
+
 ?>
+
---
+++ frontend/signup.php
@@ -0,0 +1,26 @@
+<form id="inscription" action="inscription.php" method="POST">
+    <table>
+        <tr>
+            <th>Adresse mail :</th>
+            <td><input type="text" id="inputLogin"></td>
+        </tr>
+        <tr>
+            <th>Mot de passe :</th>
+            <td><input type="password" name="inputPassword"></td>
+        </tr>
+        <tr>
+            <th>Date de Naissance :</th>
+            <td><input type="date" name="inputBirthday"></td>
+        </tr>
+        <tr>
+            <th>Sexe :</th>
+            <td>
+                <!-- TODO -->
+            </td>
+        </tr>
+        <tr>
+            <th></th>
+            <td><input type="submit" value="S'inscrire" /></td>
+        </tr>
+    </table>
+</form>
