1c7f158ca08e624add84337805787c8006016e2b
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-03-29 02:25:52 +0200

    update frontend (liste aliments)

--- frontend/aliments.php
+++ frontend/aliments.php
@@ -1,1 +1,26 @@
-<!-- TODO -->
+<table id="aliments" class="display">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Nom</th>
+            <th>Type</th>
+            <th>Calories</th>
+        </tr>
+    </thead>
+</table>
+
+
+<script>
+    $(document).ready(function () {
+        $('#aliments').DataTable({
+            'order': [],
+            'ajax': '../backend/aliments.php',
+            'colums': [
+                {"aliments":"id_aliment"},
+                {"aliments":"nom"},
+                {"aliments":"type"},
+                {"aliments":"nb_calories"}
+            ]
+        });
+    });
+</script>
