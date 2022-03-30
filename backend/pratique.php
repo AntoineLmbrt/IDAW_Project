133b1a01bb57bd114c65b32966d0aeab536ff3f5
poirotman <alexis.poirot19@gmail.com>
2022-03-30 19:04:57 +0200

    REQ diagramme calorie toute durée

--- backend/pratique.php
+++ backend/pratique.php
@@ -17,12 +17,28 @@
     if($conn->connect_error){
         die('Erreur : ' .$conn->connect_error);
     }
+
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            switch($_GET['time']){
+                case 'day':
+                    Sport_Calorie_day($conn);
+                break;
+                
+                case 'week':
+                    Sport_Calorie_week($conn);
+                break;
+
+                case 'month':
+                    Sport_Calorie_month($conn);
+                break;
+            }
+        break;
+    }
     
 
-    function Calorie_sport_day($conn){}
-
-        
-        // Gets repas du jour
+    function Sport_Calorie_day($conn){
+        // Gets sport du jour
         
         $sql="SELECT sport.nb_calories*pratique.temps/60 FROM pratique 
         LEFT JOIN sport ON sport.id_sport=pratique.id_sport 
@@ -35,4 +51,42 @@
             $nbCalSport= $nbCalSport + $row['sport.nb_calories*pratique.temps/60'];
         }
         echo $nbCalSport;  
+    }
+
+    function Sport_Calorie_week($conn){
+        // Gets sport de la semaine
+        $date_ajd=date('Y-m-d');
+        $date_db_semaine=date('Y-m-d', strtotime("this week"));
+
+        $sql="SELECT sport.nb_calories*pratique.temps/60 FROM pratique 
+        LEFT JOIN sport ON sport.id_sport=pratique.id_sport 
+        WHERE pratique.login = '".$_GET['login']."'  AND pratique.date <= '".$date_ajd."' AND pratique.date >= '".$date_db_semaine."'";
+
+        $res=$conn -> query($sql);
+        $rows = array();
+        $nbCalSport=0;
+        while($row = $res->fetch_assoc()) { 
+            $nbCalSport= $nbCalSport + $row['sport.nb_calories*pratique.temps/60'];
+        }
+        echo $nbCalSport;  
+    }
+    function Sport_Calorie_month($conn){
+        // Gets sport du mois
+        $date_ajd=date('Y-m-d');
+        $date_db_month=date('Y-m-d', strtotime("first day of this month"));
+
+        $sql="SELECT sport.nb_calories*pratique.temps/60 FROM pratique 
+        LEFT JOIN sport ON sport.id_sport=pratique.id_sport 
+        WHERE pratique.login = '".$_GET['login']."'  AND pratique.date <= '".$date_ajd."' AND pratique.date >= '".$date_db_month."'";
+
+        $res=$conn -> query($sql);
+        $rows = array();
+        $nbCalSport=0;
+        while($row = $res->fetch_assoc()) { 
+            $nbCalSport= $nbCalSport + $row['sport.nb_calories*pratique.temps/60'];
+        }
+        echo $nbCalSport;  
+    }
+
+
 ?>
--- backend/repas.php
+++ backend/repas.php
@@ -16,11 +16,30 @@
     if($conn->connect_error){
         die('Erreur : ' .$conn->connect_error);
     }
+
+    switch($_SERVER['REQUEST_METHOD']){
+        case 'GET':
+            switch($_GET['time']){
+                case 'day':
+                    
+                    Repas_Calorie_day($conn);
+                    
+                break;
+                
+                case 'week':
+                    Repas_Calorie_week($conn);
+                break;
+
+                case 'month':
+                    Repas_Calorie_month($conn);
+                break;
+            }
+        break;
+    }
     
 
     function Repas_Calorie_day($conn){
 
-        // 3 GETs calorie consommé, utilisé, total du jour
         // Gets repas du jour
         $sql="SELECT aliment.nb_calories*repas.quantite FROM repas 
         LEFT JOIN aliment ON aliment.id_aliment=repas.id_aliment 
@@ -34,5 +53,40 @@
         }
         echo $nbCalRepas;  
     
+    }
+
+    function Repas_Calorie_week($conn){
+        // gestion de la date
+        $date_ajd=date('Y-m-d');
+        $date_db_semaine=date('Y-m-d', strtotime("this week"));
+
+        $sql="SELECT aliment.nb_calories*repas.quantite FROM repas 
+        LEFT JOIN aliment ON aliment.id_aliment=repas.id_aliment 
+        WHERE repas.login = '".$_GET['login']."'  AND repas.date <= '".$date_ajd."' AND repas.date >= '".$date_db_semaine."'";
+
+        $res=$conn -> query($sql);
+        $rows = array();
+        $nbCalRepas=0;
+        while($row = $res->fetch_assoc()) { 
+            $nbCalRepas= $nbCalRepas + $row['aliment.nb_calories*repas.quantite'];
+        }
+        echo $nbCalRepas;
+    }
+
+    function Repas_Calorie_month($conn){
+        $date_ajd=date('Y-m-d');
+        $date_db_month=date('Y-m-d', strtotime("first day of this month"));
+
+        $sql="SELECT aliment.nb_calories*repas.quantite FROM repas 
+        LEFT JOIN aliment ON aliment.id_aliment=repas.id_aliment 
+        WHERE repas.login = '".$_GET['login']."'  AND repas.date <= '".$date_ajd."' AND repas.date >= '".$date_db_month."'";
+
+        $res=$conn -> query($sql);
+        $rows = array();
+        $nbCalRepas=0;
+        while($row = $res->fetch_assoc()) { 
+            $nbCalRepas= $nbCalRepas + $row['aliment.nb_calories*repas.quantite'];
+        }
+        echo $nbCalRepas;
     }
 ?>
