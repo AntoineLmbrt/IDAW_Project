bfdb42afedeb0338ea5aac2baef826df4f7628c2
poirotman <alexis.poirot19@gmail.com>
2022-04-01 23:33:47 +0200

    Ajout des nutriments et function ADD

--- backend/aliments.php
+++ backend/aliments.php
@@ -4,12 +4,13 @@
     $auth['dataBase']["username"]; 
     $conn = new mysqli($auth['dataBase']["servername"],$auth['dataBase']["username"], $auth['dataBase']["password"], $auth['dataBase']["dbname"]);
 
-    switch($_SERVER('REQUEST_METHOD')){
+    switch($_SERVER['REQUEST_METHOD']){
         case 'GET':
             listeAliments($conn);
         break;
 
         case 'POST':
+            
             if(isset($_POST['function'])){
                 switch($_POST['function']){
                     case 'UPDATE':
@@ -23,19 +24,38 @@
             }
     }
 
+    //Obtenir la liste des aliments et leurs nutriments !
     function listeAliments($conn){
         $sql = "SELECT * FROM aliment";
         $result = $conn->query($sql);
 
+        $nb_nutriment=$conn->query("SELECT COUNT(*) FROM nutriment");
+        $nb_nutriment=$nb_nutriment->fetch_assoc();
+        $nb_nutriment=$nb_nutriment['COUNT(*)'];
+
         $response=array();
+
         while($row = $result->fetch_assoc()) {
-            $rows[] = $row;
+            //On ajoute les nutriments !
+                $row2=$row;
+                // On récupère le ratio du nutriment et le nom
+                $res1=$conn->query('SELECT contient.ratio, nutriment.nom FROM contient LEFT JOIN nutriment ON contient.id_nutriment=nutriment.id_nutriment WHERE id_aliment='.$row['id_aliment']);
+                while($res=$res1->fetch_assoc()){
+                    $ratio=$res['ratio'];
+                    $nom_nutriment=$res['nom'];
+                    // On l'ajoute à la colonne
+                    $row2[$nom_nutriment]=$ratio;
+                }
+            $rows[] = $row2;
         }
-        
         $rows = mb_convert_encoding($rows,'UTF-8', 'CP1252');
         $response["draw"]= 1;
-        $response["recordsTotal"]= 3185;
-        $response["recordsFiltered"]= 3185;
+
+        // On recherche le nombre d'entrée (utile pour dataTables)
+        $row=$conn -> query("SELECT COUNT(*) FROM aliment");
+        $row=$row->fetch_assoc();
+        $response["recordsTotal"]= $row['COUNT(*)'];
+        $response["recordsFiltered"]= $row['COUNT(*)'];
         $response['data']=$rows;
         echo json_encode($response);
     }
@@ -56,7 +76,8 @@
     }
 
     function ajouterAliment($conn){
-        foreach($_POST as $key => $value){
+        $sql='';
+        foreach($_POST['aliment'] as $key => $value){
             if($key == 'nb_calories'){
                 $sql=$sql.",".$value;
             }else{
@@ -64,8 +85,16 @@
             }
         }
         $sql="INSERT INTO aliment VALUES(NULL".$sql.")";
-        $conn->query($sql);
-        $response='success';
+        $sql = mb_convert_encoding($sql, 'CP1252','UTF-8');
+        if($conn->query($sql) === TRUE){
+            $id_aliment=$conn->insert_id;
+        };
+        $response="success";
+        foreach($_POST['nutriment'] as $key => $value){
+            if($conn->query("INSERT INTO contient VALUES(".$key.",".$id_aliment.",".$value.")")==FALSE){
+                $response="failed";
+            };
+        }
         echo json_encode($response);
     }
 ?>
--- frontend/aliments.php
+++ frontend/aliments.php
@@ -5,6 +5,16 @@
             <th>Nom</th>
             <th>Type</th>
             <th>Calories</th>
+            <th>Protéines (g/100g)</th>
+            <th>Glucides (g/100g)</th>
+            <th>Lipides (g/100g)</th>
+            <th>Glucose (g/100 g)</th>
+            <th>Lactose (g/100 g)</th>
+            <th>Alcool (g/100 g)</th>
+            <th>Cholestérol (mg/100 g)</th>
+            <th>Sel chlorure de sodium (g/100 g)</th>
+            <th>Calcium (mg/100 g)</th>
+            <th>Sucres (g/100 g)</th>
         </tr>
     </thead>
 </table>
@@ -33,7 +43,17 @@
                     {data:"id_aliment"},
                     {data:"nom"},
                     {data:"type"},
-                    {data:"nb_calories"}
+                    {data:"nb_calories"},
+                    {data:"Protéines (g/100g) "},
+                    {data:"Glucides (g/100g)"},
+                    {data:"Lipides (g/100g)"},
+                    {data:"Glucose (g/100 g)"},
+                    {data:"Lactose (g/100 g)"},
+                    {data:"Alcool (g/100 g)"},
+                    {data:"Cholestérol (mg/100 g)"},
+                    {data:"Sel chlorure de sodium (g/100 g)"},
+                    {data:"Calcium (mg/100 g)"},
+                    {data:"Sucres (g/ 100g)"},
                 ],
             });
         }).fail(function(){
