3c5ad3653247258df40966bbf572994b0a67e24e
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-04-03 18:46:58 +0200

    update frontend (optimisation du css)

--- frontend/header.php
+++ frontend/header.php
@@ -9,7 +9,13 @@
         <!-- CSS files -->
         <link rel="stylesheet" href="//cdn.datatables.net/1.11.5/css/jquery.dataTables.min.css">
         <link rel="stylesheet" href="https://cdn.datatables.net/buttons/2.2.2/css/buttons.dataTables.min.css">
+
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/navigation.css">
+        <link rel="stylesheet" href="css/forms.css">
+        <link rel="stylesheet" href="css/dashboard.css">
+        <link rel="stylesheet" href="css/profile.css">
+        <link rel="stylesheet" href="css/tables.css">
 
         <!-- JS files -->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
