61b0359127d7c38632cad84878aae93d6472c4da
Antoine Lambert <antoine.lambert@etu.imt-nord-europe.fr>
2022-04-03 04:15:27 +0200

    update frontend (profile page added)

--- frontend/profil.php
+++ frontend/profil.php
@@ -1,5 +1,33 @@
-<!-- TODO -->
+<h1 id="title">Profil</h1>
+<div id="profile">
+    <div class="box">
+        <table>
+            <tr>
+                <td class="profile-key">Nom :</td>
+                <td class="profile-value">POIROT</td>
+            </tr>
+            <tr>
+                <td class="profile-key">Prénom :</td>
+                <td class="profile-value">Alexis</td>
+            </tr>
+            <tr>
+                <td class="profile-key">Email :</td>
+                <td class="profile-value">alexis.poirot@blablabla.com</td>
+            </tr>
+            <tr>
+                <td class="profile-key">Date de naissance :</td>
+                <td class="profile-value">01/01/0101</td>
+            </tr>
+            <tr>
+                <td class="profile-key">Genre :</td>
+                <td class="profile-value">Homme</td>
+            </tr>
+        </table>
+    </div>
+</div>
+
 <script>
+
     $('body').ready(chargementDonneeProfil());
 
     function chargementDonneeProfil(){
